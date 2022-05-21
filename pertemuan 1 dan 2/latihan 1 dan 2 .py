60b44b9a18420d843207a53a45e463d66971dcc7
DanuTirta004 <104290855+DanuTirta004@users.noreply.github.com>
2022-05-21 22:53:51 +0700

    Create latihan 1 dan 2 .py

---
+++ pertemuan 1 dan 2/latihan 1 dan 2 .py
@@ -0,0 +1,7 @@
+nama = "Robert Downey Jr"
+umur = 57
+tempat_tinggal = "malibu,california"
+pekerjaan = "aktor"
+gender = "laki-laki"
+
+print("nama\t\t:",nama,"\ngender\t\t:",gender,"\numur\t\t:",umur,"\ntempat tinggal\t:",tempat_tinggal,"\npekerjaan\t:",pekerjaan)
